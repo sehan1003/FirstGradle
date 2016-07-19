7d04728c5633239e6f18a5c9b8dd03c0370a447b
sh1003 <sehan1003@naver.com>
2016-07-19 16:25:22 +0900

    Hello Label add..

--- src/main/java/com/hybird/MainApplication.java
+++ src/main/java/com/hybird/MainApplication.java
@@ -3,7 +3,7 @@ package com.hybrid;
 public class MainApplication {
 	public static void main(String[] args){
 		System.out.println("#######################");
-		System.out.println("Hello First Gradle !!!");
+		System.out.println("Hello First Gradle Home Update!!!");
 		System.out.println("#######################");
 	}
 }
